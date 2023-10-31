049e98fc2155947e9453b19a8123825e4b6a3070
Yelro <50973739+Yelro@users.noreply.github.com>
2023-10-31 13:58:14 -0500

    #4 crear función para restar (#5)

---
+++ test/number-utils.test.ts
@@ -0,0 +1,7 @@
+import {subtraction} from '../src/number-utils';
+
+describe('subtraction', () => {
+  it('should return subtraction of two numbers', () => {
+    expect(subtraction(1, 1)).toEqual(1);
+  });
+});
